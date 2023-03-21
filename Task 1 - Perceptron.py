22bcf03bb4412e0508fa085677a21cce3d7f5911
aditya3103 <85363167+aditya3103@users.noreply.github.com>
2023-03-22 00:39:35 +0530

    Add files via upload

--- Task 1 - Perceptron.py
+++ Task 1 - Perceptron.py
@@ -5,11 +5,34 @@ import matplotlib.pyplot as mp
 def normalization(col):
     return (col-col.mean())/col.std()
 
+def perceptron(traindata):
+
+    y = traindata[traindata.columns[1]]
+    result = {'B': 1, 'M': -1}
+    y.replace(result, inplace=True)
+    y = y.to_numpy()
+
+    x = traindata[traindata.columns[2:]]
+    x = x.to_numpy()
+
+    w = np.zeros(len(x[0]))
+    eta = 1
+    epochs = 20
+
+    for t in range(epochs):
+            for i in range(len(x)):
+                if (np.dot(x[i], w)*y[i]) <= 0:
+                    w = w + eta*x[i]*y[i]
+    
+    return w
+
 
 # Data Preprocessing
 
 df = pd.read_csv('dataset.csv')
 df = df.fillna(df.mean(numeric_only=True))
+
+df1 = df.copy()
 
 for col in df.columns:
     if(col=='diagnosis'):
@@ -18,10 +41,25 @@ for col in df.columns:
 
 # Splitting Training and Testing Data
 
-train = df.sample(frac=0.67, random_state=np.random.randint(1,200))
-test = df.drop(train.index)
+
+# Perceptron Algorithm
+
+
 
 # Perceptron Model 1
+
+w1 = perceptron(df1.sample(frac=0.67, random_state=333))
+w2 = perceptron(df1.sample(frac=0.67, random_state=123))
+w3 = perceptron(df.sample(frac=0.67, random_state=241))
+w4 = perceptron(df[list(df.columns[:3]) + list(df.columns[np.random.permutation(np.arange(3, len(df.columns)))])].sample(frac=0.67, random_state=241))
+
+print(w1)
+print(w2)
+print(w3)
+print(w4)
+
+
+
 
 # Perceptron Model 2
 
